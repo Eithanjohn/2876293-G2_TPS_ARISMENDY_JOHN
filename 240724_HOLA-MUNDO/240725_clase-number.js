42c846fde0294bcedba5914ddc395b93f21fbd4c
Eithanjohn <johneithan123@gmail.com>
2024-07-25 11:43:04 -0500

    creacion del archivo clase number

---
+++ 240724_HOLA-MUNDO/240725_clase-number.js
@@ -0,0 +1,40 @@
+//tipos de datos primitivos
+// tipo entero y decimal
+const entero =42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal)
+
+//2.notacion  cientifica
+const cientifico =5e3
+
+// infinitos y NaN
+
+//operaciones aritmeticas
+
+//1. suma, resta, multiplicacion y division
+
+const suma = 3 + 4
+const resta = 4 - 4
+const multiplicacion = 4 * 7
+const division = 8 / 7
+
+
+//2. modulo y exponenciacion
+
+const modulo = 15 % 8
+const exponenciacion = 2 ** 3
+
+//Precision
+const resultado = 0.1 + 0.2
+console.log (resultado)
+console.log (resultado.toFixed(1))
+console.log (resultado === 0.3)
+ 
+// operaciones avanzadas
+ const raizcuadrada =Math.sqrt (16) // = asigna lo del lado derecho al lado izquierdo
+ const valorAbsoluto =Math.abs (-7)
+ const aleatorio = Math.random()
+ console.log (raizcuadrada)
+ console.log (valorAbsoluto)
+ console.log (aleatorio)
+ 
