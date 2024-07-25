eb41c005adfebbb1cdd4fd56bf033755bddc5177
Eithanjohn <johneithan123@gmail.com>
2024-07-25 12:52:22 -0500

    creacion de carpeta ejecucionCondicional y la carpeta if y su archivo index.js

---
+++ 240725_CURSO-JS/ciclos/operadores/index.js
@@ -0,0 +1,20 @@
+/*
+Operadores de comparacion
+*/
+
+//   = es asignar
+//   == es igual a la comparacion
+//   ===
+//  != es diferente
+//  !== buscamos una negacion
+
+//> mayor que
+//< menor que
+//>= mayor o igual que
+//<= menor o igual que
+
+const a =10;
+const b = 20;
+const c = "10";
+
+a== b
---
+++ 240725_CURSO-JS/ciclos/operadores/logica.js
@@ -0,0 +1,11 @@
+/*
+Operadores Logicos
+*/
+
+// && = and o Y
+// || = or o o
+// ! = not o negacion
+
+const a =10;
+const b =20;
+const c ="10"
