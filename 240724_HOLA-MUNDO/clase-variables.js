6834119c24bc93fc7f0ea21ab6a2dcd2e281c9e8
Eithanjohn <johneithan123@gmail.com>
2024-07-29 10:39:18 -0500

    creacion de nuevas carpetas en el ciclos y creacion de la nueva carpeta platzi

--- 240724_HOLA-MUNDO/clase-variables.js
+++
@@ -1,7 +0,0 @@
-let cajadeandy ="Woody"
-console.log(cajadeandy)
-
-//Lo NO permitido
-let c= "Woody"
-let cda ="Woody"
-let pcAndy ="Woody"
---
+++ 240725_CURSO-JS/ciclos/ejecucionCondicional/if/index.js
@@ -0,0 +1,21 @@
+/*if (let === algo){
+    codigo
+} else if{
+    codigo
+}else{
+    //codigo si ninguna funcion se cumple
+}*/
+
+let nombre= "Luna";
+
+
+if (nombre==="Diego"){
+    console.log("Hola Diego");
+
+}else if(nombre==="Luna")
+    {
+    console.log("hola Luna");
+}
+else{
+    console.log("nombre no encontrado");
+}
