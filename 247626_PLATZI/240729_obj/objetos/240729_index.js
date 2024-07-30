0e7029fd860293baee8d9241bd493fb3335dd5d1
Eithanjohn <johneithan123@gmail.com>
2024-07-30 06:18:21 -0500

    creacion de archivos en la carpeta if y adicion de archivos javascript y anexo del proyecto

---
+++ 247626_PLATZI/240729_obj/objetos/240729_index.js
@@ -0,0 +1,21 @@
+/*
+estructura de datos
+key / value
+objeto {
+propiedad: valor,
+propiedda: valor,
+propiedad: valor
+metodos}
+*/
+const persona ={
+    nombre: 'John',
+    edad: 30,
+    direccion: {
+        calle: '88 #72a-59',
+        ciudad: "cdbogota",
+    },
+    saludar (){
+        console.log(`hola, minombre es ${persona}`)
+    }
+
+}
